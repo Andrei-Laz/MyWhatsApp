4136b93145647bd360bf33cdacc76a2c8925d681
LAZAR <andlaz2@a2-07.infor.caminas>
2025-12-01 20:50:01 +0100

    insert of contacts: failed

--- app/src/main/java/com/example/mywhatsapp/MainActivity.kt
+++ app/src/main/java/com/example/mywhatsapp/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
@@ -38,6 +40,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -152,6 +155,21 @@ fun ChatScreen() {
         text = "Chat tab  selected",
         style = MaterialTheme.typography.bodyLarge,
     )
+
+    LazyColumn{
+        ContactCard()
+    }
+}
+
+@Composable
+fun ContactCard() {
+    Row {
+        Image(
+            painter = painterResource(R.drawable.tenma),
+            contentDescription = "Tenma"
+        )
+        Text(text = "Matsukaze Tenma")
+    }
 }
 
 @Composable
